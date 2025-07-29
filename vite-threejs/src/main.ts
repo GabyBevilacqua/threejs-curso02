09ffa6e4ec79b0dcad8681c2621634eea997b5f7
Gabriella Bevilacqua <bevi.gaby@gmail.com>
2025-07-29 16:21:59 +0200

    camios

--- vite-threejs/src/main.ts
+++ vite-threejs/src/main.ts
@@ -1,10 +1,59 @@
-import { GUI } from 'dat.gui'
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import Stats from 'three/addons/libs/stats.module.js'
 
 const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.z = 1.5
+
+const canvas = document.getElementById('canvas') as HTMLCanvasElement
+const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true })
+renderer.setSize(window.innerWidth, window.innerHeight)
+//document.body.appendChild(renderer.domElement)
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+})
+
+new OrbitControls(camera, renderer.domElement)
+
+const geometry = new THREE.BoxGeometry()
+const material = new THREE.MeshNormalMaterial({ wireframe: true })
+
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
+
+function animate() {
+  requestAnimationFrame(animate)
+
+  renderer.render(scene, camera)
+}
+
+animate()
+
+/*
+
+
+//const scene = new THREE.Scene()  // al usar una sola escena y se pueden usar los ejemplos
+// scene.background = new THREE.Color(0x123456) // Set a background color
+// scene.background = new THREE.TextureLoader().load('https://sbcode.net/img/grid.png') // Set a background texture imagen
+//scene.background = new THREE.CubeTextureLoader()
+//.setPath('https://sbcode.net/img/')
+//.load(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png']) // es una imagen dividida en las capas de un cubo
+//scene.backgroundBlurriness = 0.5 // Set the blurriness of the background texture
+
+const sceneA = new THREE.Scene()
+sceneA.background = new THREE.Color(0x123456)
+
+const sceneB = new THREE.Scene()
+sceneB.background = new THREE.TextureLoader().load('https://sbcode.net/img/grid.png')
+
+const sceneC = new THREE.Scene()
+sceneC.background = new THREE.CubeTextureLoader().setPath('https://sbcode.net/img/').load(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png'])
+//sceneC.backgroundBlurriness = 0.5
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 1.5
@@ -25,12 +74,33 @@ const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshNormalMaterial({ wireframe: true })
 
 const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+sceneA.add(cube)
+//scene.add(cube) // este cuando es una unica escena
 
 const stats = new Stats()
 document.body.appendChild(stats.dom)
 
+let activeScene = sceneA
+const setScene = {
+  sceneA: () => {
+    activeScene = sceneA
+  },
+  sceneB: () => {
+    activeScene = sceneB
+  },
+  sceneC: () => {
+    activeScene = sceneC
+  },
+}
+
 const gui = new GUI()
+
+const sceneFolder = gui.addFolder('Scenes')
+sceneFolder.add(setScene, 'sceneA').name('Scene A')
+sceneFolder.add(setScene, 'sceneB').name('Scene B')
+sceneFolder.add(setScene, 'sceneC').name('Scene C')
+
+
 const cubeFolder = gui.addFolder('Cube')
 cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2).name('Rotate X')
 cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2).name('Rotate Y')
@@ -44,16 +114,8 @@ scaleFolder.add(cube.scale, 'z', 0, 2).name('Scale Z')
 const cameraFolder = gui.addFolder('Camera')
 cameraFolder.add(camera.position, 'z', 0, 5).name('Camera Z')
 
-function animate() {
-  requestAnimationFrame(animate)
 
-  cube.rotation.x += 0.01
-  cube.rotation.y += 0.01
-  
-  renderer.render(scene, camera)
 
-  stats.update()
-}
 
-animate()
 
+*/ 
